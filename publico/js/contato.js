7479ab8815e9f30fbdec07a7f8979b6f4195a6f4
Thais Schmidt <thaisachmidtdasilva@gmail.com>
2023-10-13 17:01:13 -0300

    pagina HOME finalizada - iniciando pagina CONTATO

--- publico/js/contato.js
+++ publico/js/contato.js
@@ -1,1 +1,24 @@
-// alert(`Hello World`);
+// alert(`Pagina Contato`);
+
+//Inicio - transiçao do nav para branco ao descer a pagina 
+//Funcionando junto ao css
+
+const menu = document.querySelector('nav');
+
+function activeScroll() {
+
+    menu.classList.toggle('ativo', scrollY > 0);
+
+}
+
+window.addEventListener('scroll', activeScroll);
+
+/* Fim*/
+
+// Funçao do botao que leva para a pagina Home
+
+function caminhoHome() {
+    window.location.href = "file:///c%3A/Users/thais/OneDrive/%C3%81rea%20de%20Trabalho/projetoNode/publico/html/home.html";
+}
+
+/* Fim*/
--- publico/js/home.js
+++ publico/js/home.js
@@ -1,1 +1,28 @@
-// alert(`Hello World`);
+alert(`Pagina Home`);
+
+//Inicio - transiçao do nav para branco ao descer a pagina
+//Funcionando junto ao css - linhas 39-46
+
+const menu = document.querySelector('nav');
+
+function activeScroll() {
+    menu.classList.toggle('ativo', scrollY > 0);
+}
+
+window.addEventListener('scroll', activeScroll);
+
+//Fim
+
+// Funçao do botao que leva para a pagina de Cadastro
+
+function caminhoCadastro() {
+    window.location.href = "file:///c%3A/Users/thais/OneDrive/%C3%81rea%20de%20Trabalho/projetoNode/publico/html/cadastro.html";
+}
+
+//Funçao do botao que leva para a pagina de Contato
+
+function caminhoContato() {
+    window.location.href = "file:///c%3A/Users/thais/OneDrive/%C3%81rea%20de%20Trabalho/projetoNode/publico/html/contato.html";
+}
+
+//Fim
